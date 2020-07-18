db7e313c3ebbdf76d8a4765d5287646556caf5f5
Emmanueljoseph-tech <emmanueljosephe.joe@yahoo.com>
2020-07-18 21:00:40 +0100

    Activity Login page

--- app/src/main/java/com/example/portal/MainActivity.java
+++ app/src/main/java/com/example/portal/MainActivity.java
@@ -10,44 +10,39 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements PortalJava {
+    private EditText etEmail, etPassword;
+    private Button btnLogin;
+    private TextView tvForgot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        private final String email, password;
+
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        tvForgot = findViewById(R.id.tvForgot);
+
+        btnLogin.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v){
+                email = etEmail.getText().toString().trim();
+                password = etPassword.getText().toString().trim();
+
+                validate();
             }
         });
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void validate() {
+        if(email.isEmpty){
+            etEmail.setError("Email cannot be empty");
+        }else is(password.isEmpty)
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
